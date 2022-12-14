04f45337e9d8a2901e7161d250c5a892f4cb0487
pierrepintoo <p.pinto2oliveira@gmail.com>
2022-12-14 09:52:49 +0100

    :sparkles: Add orbit controls and polygon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Box from './Box';
-import { Canvas } from '@react-three/fiber';
 import Plane from './Plane'
+import { useRef } from 'react';
+import { Canvas, useFrame, useThree, extend } from '@react-three/fiber';
+import { OrbitControls, TransformControls } from 'three-stdlib'
+
+extend({ OrbitControls, TransformControls })
+
 
 function App() {
+
+  const Controls = () => {
+    const controls = useRef()
+    const { camera, gl } = useThree()
+    useFrame(() => controls.current.update())
+    return <orbitControls ref={controls} args={[camera, gl.domElement]} enableDamping dampingFactor={0.1} rotateSpeed={0.5} />
+  }
+
   return (
     <div className='canvas'>
       <Canvas camera={{
-        position: [0, 0, -5]
-        }}
-        orthographic
+        position: [0, 0, -30]
+      }}
+        // orthographic
       >
+        <Controls />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
--- src/Plane.jsx
+++ src/Plane.jsx
@@ -10,15 +10,29 @@ const Plane = (props) => {
 
     const { viewport } = useThree()
 
+    const x = 0, y = 0;
+
+    const heartShape = new THREE.Shape();
+
+    // heartShape.moveTo( x + 5, y + 5 );
+    heartShape.lineTo( x + 10, y - 5)
+    heartShape.quadraticCurveTo( x + 11, y - 6)
+    // heartShape.lineTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+    // heartShape.lineTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+    // heartShape.lineTo( x + 16, y + 7, x + 16, y, x + 10, y );
+    // heartShape.lineTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+    // heartShape.lineTo(x + 10, y + 10)
+    // heartShape.lineTo(x + 3, y + 3)
+
     return (
         <mesh
             {...props}
             // position={[3, 0, 2]}
             rotation={new THREE.Euler(3.14, 0, 0)}
-            scale={[viewport.width, viewport.height, 1]}
+            scale={[1, 1, 1]}
         >
-            <planeBufferGeometry attach="geometry" args={[1, 1, 100, 100]} wireframe />
-            <meshStandardMaterial attach="material" color='green' wireframe wireframeLinewidth={1}/>
+            <shapeGeometry  args={[heartShape]} />
+            <meshStandardMaterial attach="material" color='green' />
         </mesh>
     )
 }
